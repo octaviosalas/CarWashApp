62eae6f9021a58ab377374a84c06f6d644dd67fa
Octavio Salas <salasoctavio129@gmail.com>
2024-06-09 23:22:53 -0300

    editingCleaning

--- frontend/frontend/src/components/Cleanings/EditJobForm.tsx
+++ frontend/frontend/src/components/Cleanings/EditJobForm.tsx
@@ -15,39 +15,19 @@ interface Props {
 const EditJobForm = ({detail, clients, goBack}: Props) => {
 
   const [amount, setAmount] = useState(detail.amount)
-  const [typeOfJob, setTypeOfJob] = useState(detail.typeOfJob)
-  const [status, setStatus] = useState(detail.status)
-  const [paid, setPaid] = useState(detail.paid)
-  const [deletePaid, setDeletePaid] = useState<boolean>(false)
   const user = userStore(state => state.user)
-  const userClients = userStore(state => state.userClients)
 
-  console.log(user, userClients)
 
   const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>) => { 
     setAmount(Number(e.target.value))
   }
 
-  const handleChangeStatus= (e: React.ChangeEvent<HTMLInputElement>) => { 
-    setStatus(e.target.value)
+  const handleChangeClient = (e: React.ChangeEvent<HTMLInputElement>) => { 
+    setAmount(Number(e.target.value))
   }
 
-  const handleChangePaid = (e: React.ChangeEvent<HTMLInputElement>) => { 
-    console.log(e.target.value)
-    if(e.target.value === "pending") { 
-      setPaid(false)
-    } else { 
-      setPaid(true)
-    }
-  }
 
-  useEffect(() => {  
-    if(paid === false) { 
-      setDeletePaid(true)
-    } else { 
-      setDeletePaid(false)
-    }
-  }, [paid])
+
 
 
   return (
@@ -59,31 +39,22 @@ const EditJobForm = ({detail, clients, goBack}: Props) => {
 
         <div className='flex flex-col justify-start text-start items-start'>
             <p className='text-sm text-black font-medium'>Monto del Lavado</p>
-              <input type="text" name="price" id="price" className=" mt-1s w-40 xl:w-52 2xl:w-96 rounded-md border-1 py-1.5 pl-7 pr-20 sm:text-sm sm:leading-6 focus:outline-none" 
+              <input type="text" name="price" id="price" className=" mt-1s w-72 xl:w-80 2xl:w-96 rounded-md border-1 py-1.5 pl-7 pr-20 sm:text-sm sm:leading-6 focus:outline-none" 
                 value={amount}
                 onChange={handleChangeAmount}
               />
         </div>
 
         <div className='flex flex-col justify-start text-start items-start mt-4'>
-            <p className='text-sm text-black font-medium'>Estado del Lavado</p>
-            <Autocomplete aria-label="Status" defaultItems={status} defaultSelectedKey={status} className="max-w-xs" onChange={handleChangeStatus}>
-                <AutocompleteItem key="pending">Pendiente</AutocompleteItem>
-                <AutocompleteItem key="completed">Completado</AutocompleteItem>
-            </Autocomplete>
+            <p className='text-sm text-black font-medium'>Cliente</p>
+            <input type="text" name="price" id="price" className=" mt-1s w-72 xl:w-80 2xl:w-96 rounded-md border-1 py-1.5 pl-7 pr-20 sm:text-sm sm:leading-6 focus:outline-none" 
+                value={detail.client.name}
+                onChange={handleChangeClient}
+              />
         </div>
 
-      
+    
 
-        <div className='flex flex-col justify-start text-start items-start mt-4'>
-            <p className='text-sm text-black font-medium'>Cobro del Lavado</p>
-            <Autocomplete   aria-label="Paid" defaultSelectedKey={paid ? "paid" : "unPaid"} className="max-w-xs" onChange={handleChangePaid}>
-                <AutocompleteItem key="paid" onClick={() => setPaid(true)}>Pagado</AutocompleteItem>
-                <AutocompleteItem key="unPaid" onClick={() => setPaid(false)}>Sin Abonar</AutocompleteItem>
-            </Autocomplete>
-        </div>
-
-        {deletePaid ? <p>a</p> : null}
 
          
       </div> 
